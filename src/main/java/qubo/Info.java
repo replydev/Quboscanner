e27f24b6b91861577daf31aa0403f3386701226c
lorenzo-patane <lorenzo.patane01@gmail.com>
2020-03-10 13:02:27 +0100

    Version 0.3.5

--- src/main/java/qubo/Info.java
+++ src/main/java/qubo/Info.java
@@ -1,8 +1,8 @@
 package qubo;
 
 public class Info {
-    public static final String version = "0.3.4";
-    public static final String otherVersionInfo = "";
+    public static final String version = "0.3.5";
+    public static final String otherVersionInfo = "BETA";
     public static int serverFound = 0;
     public static int serverNotFilteredFound = 0;
     public static boolean gui;
