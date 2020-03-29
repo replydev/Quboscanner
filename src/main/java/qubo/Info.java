a6264ac98cd2ab811cac16915d209d4ec18b470a
lorenzo-patane <lorenzo.patane01@gmail.com>
2020-03-29 22:47:25 +0200

    Updated version number

--- src/main/java/qubo/Info.java
+++ src/main/java/qubo/Info.java
@@ -1,8 +1,8 @@
 package qubo;
 
 public class Info {
-    public static final String version = "0.3.5";
-    public static final String otherVersionInfo = "BETA";
+    public static final String version = "0.3.6";
+    public static final String otherVersionInfo = "alpha";
     public static int serverFound = 0;
     public static int serverNotFilteredFound = 0;
     public static boolean gui;
