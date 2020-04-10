db5a6a49a43224872fcc7f0022fd7c647356b95a
lorenzo-patane <lorenzo.patane01@gmail.com>
2020-04-10 11:24:09 +0200

    No more alpha

--- src/main/java/qubo/Info.java
+++ src/main/java/qubo/Info.java
@@ -2,7 +2,7 @@ package qubo;
 
 public class Info {
     public static final String version = "0.3.6";
-    public static final String otherVersionInfo = "alpha";
+    public static final String otherVersionInfo = "";
     public static int serverFound = 0;
     public static int serverNotFilteredFound = 0;
     public static boolean gui;
